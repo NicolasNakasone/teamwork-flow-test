98e04680f54b1b104b7aa04bf3d20094cd652ea8
NicoConstantin <nicoconstantin11@gmail.com>
2021-06-05 19:11:29 -0300

    cambio nico

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Henry Pokemon - Rama desde espejo - CambioMaxi</h1>
+      <h1>Henry Pokemon - Rama desde espejo - CambioMaxi - Cambio Nico</h1>
     </div>
   );
 }
