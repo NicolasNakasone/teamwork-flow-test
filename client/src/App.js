cc44fc732971ee97cae3809a8de2f18e93a4fbeb
MaxiSanchez600 <maxsanchezg@gmail.com>
2021-06-05 19:32:15 -0300

    Cambio Sobre Titulo

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Henry Pokemon - Rama desde espejo - CambioMaxi</h1>
+      <h1>Henry Pokemon - Rama desde espejo - CambioMaxi - CambioMaxi2</h1>
     </div>
   );
 }
