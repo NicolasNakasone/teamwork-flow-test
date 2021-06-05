d85d45d72f4eb0eaefb62899abcfeee2de7b8d30
Jose Duarte <jdavidduart@gmail.com>
2021-06-05 17:24:51 -0500

    correccion sugerida GRACIASSS

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Henry Pokemon - Rama desde espejo - CambioMaxi-cambio x</h1>
+      <h1>Henry Pokemon - Rama desde espejo - CambioMaxi-AQUI TIENES LA MAYUS</h1>
     </div>
   );
 }
